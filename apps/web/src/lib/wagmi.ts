67c0a167914912980d74fc7d4c1e1cb078bdd706
Jay Popat <contact.jaypopat@gmail.com>
2025-11-09 06:19:03 +0000

    push updated lockfile

--- apps/web/src/lib/wagmi.ts
+++ apps/web/src/lib/wagmi.ts
@@ -8,14 +8,14 @@ export const wagmiConfig = createConfig({
 	chains: isDevelopment ? [anvil] : [sepolia],
 	connectors: isDevelopment
 		? [
-			mock({
-				accounts: [
-					"0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-					"0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
-					"0x1234567890123456789012345678901234567890",
-				],
-			}),
-		]
+				mock({
+					accounts: [
+						"0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+						"0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+						"0x1234567890123456789012345678901234567890",
+					],
+				}),
+			]
 		: [injected()],
 	transports: {
 		[anvil.id]: http("http://127.0.0.1:8545", {
