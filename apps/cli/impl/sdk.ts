840dab307424c97df9ba362b12e9d35461341ae0
Jay Popat <contact.jaypopat@gmail.com>
2025-11-19 00:32:00 +0000

    fixing server dockerfile #1

--- apps/cli/impl/sdk.ts
+++ apps/cli/impl/sdk.ts
@@ -1,22 +1,8 @@
 import { SDK } from "@zkfair/sdk";
-import type { Hash, Hex } from "viem";
-
-const isOnChain = process.env.ONCHAIN === "true";
+import type { Hex } from "viem";
 
 export const zkFairSDK = new SDK({
-	rpcUrl:
-		(isOnChain ? process.env.ONCHAIN_RPC_URL : process.env.RPC_URL) ||
-		"http://localhost:8545",
-	contractAddress:
-		((isOnChain
-			? (process.env.ONCHAIN_CONTRACT_ADDRESS as Hash)
-			: process.env.CONTRACT_ADDRESS) as Hash) || "",
-	privateKey:
-		(isOnChain
-			? (process.env.ONCHAIN_PRIVATE_KEY as Hex)
-			: (process.env.PRIVATE_KEY as Hex)) || "",
-	attestationServiceUrl:
-		(isOnChain
-			? process.env.ONCHAIN_ATTESTATION_SERVICE_URL
-			: process.env.ATTESTATION_SERVICE_URL) || "http://localhost:3000",
+	rpcUrl: process.env.RPC_URL || "http://localhost:8545",
+	contractAddress: process.env.CONTRACT_ADDRESS as Hex,
+	privateKey: process.env.PRIVATE_KEY as Hex,
 });
--- apps/server/lib/sdk.ts
+++ apps/server/lib/sdk.ts
@@ -5,6 +5,4 @@ export const sdk = new SDK({
 	contractAddress: process.env.CONTRACT_ADDRESS as Hex,
 	privateKey: process.env.PRIVATE_KEY as Hex,
 	rpcUrl: process.env.RPC_URL || "http://localhost:8545",
-	attestationServiceUrl:
-		process.env.ATTESTATION_SERVICE_URL || "http://localhost:3000",
 });
